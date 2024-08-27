4474d240440e852ac31fd2b415da9d390cae0f32
gkfktkrh153 <gkfktkrh153@naver.com>
2024-08-27 23:25:18 +0900

    init

---
+++ src/main/java/com/kcc/untitled/UntitledApplication.java
@@ -0,0 +1,13 @@
+package com.kcc.untitled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntitledApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UntitledApplication.class, args);
+	}
+
+}
