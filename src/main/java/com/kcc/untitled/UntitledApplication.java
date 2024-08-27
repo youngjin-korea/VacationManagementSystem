f8569af0953d9e905b09f1ab53be32023fa99070
gkfktkrh153 <gkfktkrh153@naver.com>
2024-08-27 23:42:27 +0900

    commit message

--- src/main/java/com/kcc/untitled/UntitledApplication.java
+++ src/main/java/com/kcc/untitled/UntitledApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UntitledApplication {
 
 	public static void main(String[] args) {
+		// PR Test
 		SpringApplication.run(UntitledApplication.class, args);
 	}
 
