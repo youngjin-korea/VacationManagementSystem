2dfd4c810190f5d49f3f72504ae4436361b6de99
kimyoungjin <youngjin1293@naver.com>
2024-09-02 14:32:38 +0900

    내 정보수정 완료

--- src/main/java/com/kcc/vacation/domain/employee/mapper/EmployeeMapper.java
+++ src/main/java/com/kcc/vacation/domain/employee/mapper/EmployeeMapper.java
@@ -1,9 +1,12 @@
 package com.kcc.vacation.domain.employee.mapper;
 
+import com.kcc.vacation.domain.employee.dto.request.UpdateMyInfo;
 import com.kcc.vacation.domain.employee.dto.response.MyInfo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface EmployeeMapper {
     MyInfo getMyInfo(int employeeId);
+    int updateMyInfo(@Param("updateMyInfo") UpdateMyInfo updateMyInfo);
 }
