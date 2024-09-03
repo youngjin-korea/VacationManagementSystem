6bc97607763edbef564067ce31c01b0dfd93deff
gkfktkrh153 <gkfktkrh153@naver.com>
2024-09-04 02:58:38 +0900

    조직관리 API연동

--- src/main/java/com/kcc/vacation/domain/department/mapper/DepartmentMapper.java
+++ src/main/java/com/kcc/vacation/domain/department/mapper/DepartmentMapper.java
@@ -1,10 +1,25 @@
 package com.kcc.vacation.domain.department.mapper;
 
+import com.kcc.vacation.domain.department.dto.request.DepartmentCreate;
+import com.kcc.vacation.domain.department.dto.request.DepartmentUpdate;
+import com.kcc.vacation.domain.department.dto.request.UpperDepartmentIdUpdate;
 import com.kcc.vacation.domain.department.dto.response.DepartmentDetail;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface DepartmentMapper {
-    DepartmentDetail getDepartment(int departmentId);
+    DepartmentDetail findDepartment(int departmentId);
+    List<DepartmentDetail> findAllDepartment();
+    void updateDepartment(DepartmentUpdate departmentUpdate);
 
+    void deleteDepartment(int id);
+
+
+    List<Integer> findUpdateTargetIds(int deleteTargetId);
+
+    void updateUpperDepartmentId(UpperDepartmentIdUpdate upperDepartmentIdUpdate);
+
+    void createDepartment(DepartmentCreate departmentCreate);
 }
