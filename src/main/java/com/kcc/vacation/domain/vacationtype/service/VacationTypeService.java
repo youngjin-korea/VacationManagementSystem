f37a45c305b849318d949569d4c2863bd8611663
haha002024 <haha002024@gmail.com>
2024-09-03 17:04:57 +0900

    휴가유형 add

--- src/main/java/com/kcc/vacation/domain/vacationtype/service/VacationTypeService.java
+++ src/main/java/com/kcc/vacation/domain/vacationtype/service/VacationTypeService.java
@@ -1,11 +1,29 @@
 package com.kcc.vacation.domain.vacationtype.service;
 
+import com.kcc.vacation.domain.vacationtype.dto.response.VacationTypeDetail;
 import com.kcc.vacation.domain.vacationtype.mapper.VacationTypeMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class VacationTypeService {
     private final VacationTypeMapper vacationTypeMapper;
+//
+//    public VacationTypeDetail getVacationType(String vactionName) {
+//        return vacationTypeMapper.getVacationType(vactionName);
+//    }
+
+    public List<VacationTypeDetail> getAllVacationTypes() {
+        return vacationTypeMapper.getAllVacationTypes();
+    }
+
+
+    public VacationTypeDetail getVacationTypeById(int id) {
+        System.out.println( "VacationTypeDetail"+ id);
+        System.out.println( "VacationTypeDetail >>>>>>>>>>>>>"+vacationTypeMapper.getVacationById(id).getMax_days());
+        return vacationTypeMapper.getVacationById(id);
+    }
 }
