d73590e218b5daf9fb428f024cf4e2dd4320bed2
221B0825 <eunseoyu0825@gmail.com>
2024-09-04 15:48:18 +0900

    feat: getVacationRequestListByApproverId

--- src/main/java/com/kcc/vacation/domain/vacationrequest/mapper/VacationRequestMapper.java
+++ src/main/java/com/kcc/vacation/domain/vacationrequest/mapper/VacationRequestMapper.java
@@ -3,6 +3,7 @@ package com.kcc.vacation.domain.vacationrequest.mapper;
 import com.kcc.vacation.domain.vacationrequest.dto.request.MyVacationRequest;
 import com.kcc.vacation.domain.vacationrequest.dto.response.MyVacation;
 import com.kcc.vacation.domain.vacationrequest.dto.response.VacationRequestDetail;
+import com.kcc.vacation.domain.vacationrequest.dto.response.VacationRequestList;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -10,10 +11,15 @@ import java.util.List;
 
 @Mapper
 public interface VacationRequestMapper {
-    VacationRequestDetail getVacationRequestDetail(Long vacationRequestId);
-    List<VacationRequestDetail> getVacationList();
-    List<MyVacation> getMyVacations(int employeeId);
+    // 관리자용 휴가 신청 자세히 보기
+    VacationRequestDetail getVacationRequestDetail(int vacationRequestId);
+    // 관리자용 휴가 신청 목록 보기
+    List<VacationRequestList> getVacationRequestListByApproverId(int approverId);
+    // 관리자용 승인 버튼 눌렀을 때
+    int updateApproverStatus(int requestId, int approverId);
 
+    List<MyVacation> getMyVacations(int employeeId);
     int insertVacationRequest(MyVacationRequest myVacationRequest);
+
 
 }
