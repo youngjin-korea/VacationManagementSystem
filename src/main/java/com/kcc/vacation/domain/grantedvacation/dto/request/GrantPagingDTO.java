b7ac6c8f2e1faeca9183a7c9121aee190c1e12e6
haha002024 <haha002024@gmail.com>
2024-09-06 16:40:52 +0900

    feat: grant search

--- src/main/java/com/kcc/vacation/domain/grantedvacation/dto/request/GrantPagingDTO.java
+++ src/main/java/com/kcc/vacation/domain/grantedvacation/dto/request/GrantPagingDTO.java
@@ -1,77 +1,52 @@
 package com.kcc.vacation.domain.grantedvacation.dto.request;
 
+import com.kcc.vacation.domain.grantedvacation.dto.response.GrantedVacationList;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.sql.Timestamp;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
-public class PagingDTO {
+@Setter
+public class GrantPagingDTO {
 
-    private int min;
-    // 최대 페이지 번호
-    private int max;
-
-    // 이전 버튼의 페이지 번호
+    private int startPage;
+    private int endPage;
+    private boolean prev;
+    private boolean next;
     private int prevPage;
-    // 다음 버튼의 페이지 번호
     private int nextPage;
+    private List<GrantedVacationList> grantedVacationList;
+    private GrantSavePagingInfo gpi;
+    private int total;
 
-    // 전체 페이지 개수
-    private int pageCnt;
-    // 현재 페이지 번호
-    private int currentPage;
 
-    // 페이지당 항목 수
-    private int contentPageCnt;
+    public GrantPagingDTO(GrantSavePagingInfo gpi, int total, List<GrantedVacationList> grantedVacationList) {
+        this.gpi = gpi;
+        this.total = total;
+        this.grantedVacationList = grantedVacationList;
 
-    // 페이지 버튼의 개수
-    private int paginationCnt;
-    
-    @Builder
-    public PagingDTO( int contentCnt, int currentPage, int contentPageCnt, int paginationCnt) {
+        /* 마지막 페이지 */
+        this.endPage = (int)(Math.ceil(gpi.getPageNum() / 5.0)) * 5;
+        /* 시작 페이지 */
+        this.startPage = this.endPage - 4;  // 5개 페이지씩 보이므로 4를 빼줍니다.
 
-        this.currentPage = currentPage;
+        /* 전체 마지막 페이지 */
+        int realEnd = (int)(Math.ceil(total * 1.0 / gpi.getAmount()));
 
-        pageCnt = contentCnt / contentPageCnt;
-        //ex) 533/10=53.3이므로 나머지가 3이므로 1page 더 필요
-        if (contentCnt % contentPageCnt > 0) {
-            pageCnt++;
+        /* 전체 마지막 페이지(realEnd)가 화면에 보이는 마지막 페이지(endPage)보다 작은 경우, 보이는 페이지(endPage) 값 조정 */
+        if(realEnd < this.endPage) {
+            this.endPage = realEnd;
         }
 
-        //--------------------------------
+        /* 시작 페이지(startPage)값이 1보다 큰 경우 true */
+        this.prev = this.startPage > 1;
 
-        //최소값 = ((현재 페이지 번호-1) / 페이지 당 글의 개수) * 페이지 당 글의 개수 + 1
-        //ex) ((1-1)/10)*10+1=1
-        //    ((2-1)/10)*10+1=2
-        //    ((3-1)/10)*10+1=3
-
-        //ex) ((4-1)/11)*11+1=4
-        //    ((5-1)/11)*11+1=5
-        min = ((currentPage - 1) / contentPageCnt) * contentPageCnt + 1;
-
-        //최대값 = min +  페이지 버튼의 개수
-        //ex) 1 + 2 - 1 = 2 (페이지 버튼의 개수:2, 최소값:1 , 페이지 당 글의 개수:10)
-        //(1,2)
-        //ex) 4 + 10 -1 = 13 (페이지 버튼의 개수:10, 최소값:4 , 페이지 당 글의 개수:11)
-        //(4,5,6,7,8,9,10,11,12,13)
-        max = min + paginationCnt - 1;
-
-        //1페이지에 1~9번째 글이 존재할경우 max=1+2-1=2, pageCnt=10/9=1.1 -> 2페이지 필요
-        //전체페이지를 넘어가지 않도록 세팅함
-        if (max > pageCnt) {
-            max = pageCnt;
-
-        }
-        prevPage = min-1;
-        //다음페이지 누를경우
-        nextPage = max+1;
-
-        //마지막페이지를 넘어가지 않도록
-        if(nextPage > pageCnt){
-            nextPage = pageCnt;
-        }
+        /* 마지막 페이지(endPage)값이 전체 마지막 페이지(realEnd)보다 작은 경우 true */
+        this.next = this.endPage < realEnd;
     }
+
 }
--- src/main/java/com/kcc/vacation/domain/grantedvacation/mapper/GrantedPagingMapper.java
+++ src/main/java/com/kcc/vacation/domain/grantedvacation/mapper/GrantedPagingMapper.java
@@ -1,4 +1,14 @@
 package com.kcc.vacation.domain.grantedvacation.mapper;
 
-public class GrantedPagingMapper {
+import com.kcc.vacation.domain.grantedvacation.dto.request.GrantSavePagingInfo;
+import com.kcc.vacation.domain.grantedvacation.dto.response.GrantedVacationList;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface GrantedPagingMapper {
+
+    List<GrantedVacationList> getGrantVacationListPaging(GrantSavePagingInfo grantSavePagingInfo);
+    int getTotalGrantedVacationCount();
 }
